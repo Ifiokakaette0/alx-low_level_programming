f25e49b1a928d5200ac76eb934350b32d4a97479
Ifiokakaette0 <akaetteifiok@gmail.com>
2022-09-14 04:01:31 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints 10 times
+ * followed by a new line
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
